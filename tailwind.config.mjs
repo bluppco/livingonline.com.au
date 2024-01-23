b1b7a7b5dfb8af78ce196032f8e7d50f88962183
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-23 11:48:05 +0530

    feat: created button component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,23 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				'lato': ["Lato","sans-serif"]
+
+			screens: {
+
+				"mac": "1440px"
+
 			},
+
+			fontFamily: {
+
+				"lato": [ "Lato","sans-serif" ]
+
+			},
+
 			colors: {
+
 				"body": "#ef6d30"
+
 			}
 		},
 	},
