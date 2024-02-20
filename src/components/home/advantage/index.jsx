e4dd6c5edac0e50eebce25ce173ea4c1cf44ef6e
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-20 11:26:45 +0530

    feat: added more description in read more button in advantage component

--- src/components/home/advantage/index.jsx
+++ src/components/home/advantage/index.jsx
@@ -10,14 +10,13 @@ import ReadMore from "@/components/common/read-more/index"
 const Advantage = ( props ) => {
 
     const { data } = props
-
-    const html = marked.parse(data.description)
+    const html = marked.parse( data.description )
 
     return(
         <div className="space-y-4 md:space-y-6">
             <HeadingFourJSX>{ data.title }</HeadingFourJSX>
             <section dangerouslySetInnerHTML={{ __html: html }}className="markdown textlg" />
-            <ReadMore text={ data.description } />
+            <ReadMore text={ data.more_description } />
         </div>
     )
 
