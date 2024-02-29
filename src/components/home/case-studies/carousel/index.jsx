55d9823983833ad598b831ef5c2bf69aafccc7ff
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-29 13:23:59 +0530

    feat: optimized case study carousel for mobile

--- src/components/home/case-studies/carousel/index.jsx
+++ src/components/home/case-studies/carousel/index.jsx
@@ -30,7 +30,7 @@ const CaseStudyCarousel = ( props ) => {
 
     return (
         <section className="w-full h-full relative">
-            <div className="h-full flex items-center absolute top-0 -left-16" onClick={ prevSlide }>
+            <div className="h-full flex items-center absolute top-0 -left-2 md:-left-16 z-10" onClick={ prevSlide }>
                 <div className="w-8 aspect-square cursor-pointer">
                     <PictureInternalContain
                         alternative_text="left icon"
@@ -38,10 +38,30 @@ const CaseStudyCarousel = ( props ) => {
                     />
                 </div>
             </div>
-            <div className="flex gap-8 transition-all duration-500">
+            <div className="gap-8 transition-all duration-500 hidden md:flex">
                 {
 
                     [ 0, 1 ].map( ( value ) => {
+
+                    const index = ( startIndex + value ) % data.length
+
+                        return (
+                            <div key={ index } className="relative w-full h-[350px] flex flex-col items-center justify-center bg-cover" style={{ backgroundImage: `url(${ CDN + PROJECT + "/" + data[ index ].cover + "?quality=80" + "&width=800" })` }}>
+                                <div className="space-y-4 px-6">
+                                    <p className="md:text-lg text-white font-lato capitalize font-bold text-center relative z-10">{ data[ index ].company_name }</p>
+                                    <p className="text-xl md:text-3xl text-white font-lato capitalize font-bold text-center relative z-10">{ data[ index ].title }</p>
+                                </div>
+                                <div className="bg-red-700/80 h-full w-full absolute top-0 left-0 opacity-90" style={{ backgroundColor: data[ index ].color_code }} />
+                            </div>
+                        )
+                    })
+
+                }
+            </div>
+            <div className="flex gap-8 transition-all duration-500 px-10 md:hidden">
+                {
+
+                    [ 0 ].map( ( value ) => {
 
                     const index = ( startIndex + value ) % data.length
 
@@ -75,7 +95,7 @@ const CaseStudyCarousel = ( props ) => {
 
                }
             </div>
-            <div className="h-full flex items-center absolute top-0 -right-16" onClick={ nextSlide }>
+            <div className="h-full flex items-center absolute top-0 -right-2 md:-right-16 z-10" onClick={ nextSlide }>
                 <div className="w-8 aspect-square cursor-pointer">
                     <PictureInternalContain
                         alternative_text="right icon"
